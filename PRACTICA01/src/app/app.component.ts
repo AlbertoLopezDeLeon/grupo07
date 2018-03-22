d606a27489801d67e0bb1b797e4e9ccdd04b7e44
Cristhian <alcantaralc3@gmail.com>
2018-03-22 22:47:16 +0100

    Mejoras en el codigo

--- PRACTICA01/src/app/app.component.ts
+++ PRACTICA01/src/app/app.component.ts
@@ -10,22 +10,34 @@ import { FormsModule } from '@angular/forms';
 export class AppComponent {
   title = 'COMANDA';
   
-  public txtNombre:string="Pepe";
-  public txtApellidos:string="Martin Manso";
-  
   private colores:Array<[string,number]>=[['Ternera con bambu y setas',7],['Cerdo agridulce',5.5],['Arroz tres delicias',3.5]];
-  public cod_color:string='verde';
-  public cod_color2=0;
+  
+  public cod_color:string='Ternera con bambu y setas';
+  public cod_color2=7;
+  
+  public element:[string,number]=[this.cod_color,this.cod_color2];
+  
+  public cadena:string =this.element[0]+" "+"("+this.element[1]+")";
   
   public lineas=[];
   
-  public addLinea(arg: string)
+  public total:number = 0;
+  
+  public addLinea()
   {
-	  this.lineas.push(arg);
+	  //this.lineas.push('Linea de ejemplo '+(this.lineas.length+1));
+
+	this.lineas.push(this.cadena.substring(0,this.cadena.indexOf("("))+" "+
+						this.cadena.substring(this.cadena.indexOf("(")+1,this.cadena.indexOf(")")));
+	let coste = + this.cadena.substring(this.cadena.indexOf("(")+1,this.cadena.indexOf("€"));
+	this.total += coste;
   }
   
   public delLinea(i)
   {
+	  let coste = + this.lineas[i].substring(this.lineas[i].indexOf(" ")+1,this.lineas[i].indexOf("€"));
+	  this.total -= coste;
 	  this.lineas.splice(i,1);
+
   }
 }
