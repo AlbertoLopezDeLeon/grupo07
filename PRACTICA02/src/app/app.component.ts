8dec058c62a47e7a73426b13aca5d62984d30cf4
CristianLUC3M <36993891+CristianLUC3M@users.noreply.github.com>
2018-04-02 18:33:06 +0200

    MiniPractica 02 - nueva version
    Correciones realizadas en la mini practica 02 de acuerdo a lo puesto por el profesor en la guía

--- PRACTICA02/src/app/app.component.ts
+++ PRACTICA02/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component,Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Vehiculo } from './vehiculo'
+import { EstadoCoche, Vehiculo } from './vehiculo'
 
 @Component({
   selector: 'app-root',
@@ -8,34 +8,46 @@ import { Vehiculo } from './vehiculo'
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent {
-  title = 'VENTA DE VEHICULOS';
+
+  title = 'Venta de coches de segunda mano';
   
   filtro = '';
   
-  vehiculos:Array<Vehiculo>=
-		[new Vehiculo('../../assets/Images/SeatLeon.jpg', 'Seat', 'Leon', new Date(2002,3,30), new Date(2003,1,1), 15000),
-		 new Vehiculo('../../assets/Images/AudiNuvolari.jpg', 'Audi', 'Nuvolari', new Date(2003,0,12), new Date(2003,1,30), 24000),
-		 new Vehiculo('../../assets/Images/AudiA4.jpg', 'Audi', 'A4', new Date(2005,4,1), new Date(2005,5,1), 18500),
-		 new Vehiculo('../../assets/Images/NoDisponible.jpg', 'BMW', 'Serie 4', new Date(2015,3,17), new Date(2015,7,1), 25000),
-		 new Vehiculo('../../assets/Images/NoDisponible.jpg', 'BMW', 'X3', new Date(2008,10,18), new Date(2009,1,2), 17000),
-		 new Vehiculo('../../assets/Images/SeatToledo.JPG', 'Seat', 'Toledo', new Date(2000,8,15), new Date(2001,2,2), 12000)];
+  public EstadoCoche:any = EstadoCoche;
+  
+  public vehiculos:Array<Vehiculo>=
+		[new Vehiculo('../../assets/Images/SeatLeon.jpg', 'Seat', 'Leon', new Date(2002,3,30), new Date(2003,1,1), 15000, this.EstadoCoche.BUENO),
+		 new Vehiculo('../../assets/Images/AudiNuvolari.jpg', 'Audi', 'Nuvolari', new Date(2003,0,12), new Date(2003,1,30), 24000, this.EstadoCoche.BUENO),
+		 new Vehiculo('../../assets/Images/AudiA4.jpg', 'Audi', 'A4', new Date(2005,4,1), new Date(2005,5,1), 18500, this.EstadoCoche.MALO),
+		 new Vehiculo('', 'BMW', 'Serie 4', new Date(2015,3,17), new Date(2015,7,1), 25000, this.EstadoCoche.MALO),
+		 new Vehiculo('', 'BMW', 'X3', new Date(2008,10,18), new Date(2009,1,2), 17000, this.EstadoCoche.BUENO),
+		 new Vehiculo('../../assets/Images/SeatToledo.JPG', 'Seat', 'Toledo', new Date(2000,8,15), new Date(2001,2,2), 12000, this.EstadoCoche.MALO)];
 		 
-  vehiculosFiltro:Array<Vehiculo> = this.vehiculos;
 		 
   public vendido(id:number)
   {
 	this.vehiculos.splice(id,1);
-	this.vehiculosFiltro = this.vehiculos;
   }
   
   public rebajar(id:number)
   {
-	this.vehiculosFiltro[id].precio = this.vehiculosFiltro[id].precio*0.9;
+	this.vehiculos[id].precio = this.vehiculos[id].precio*0.9;
   }
   
-  public filtrar()
+  public getFiltrados(): Array<number>
   {
-	this.vehiculosFiltro = this.vehiculos.filter(vehiculo => vehiculo.marca.substring(0,this.filtro.length).toLowerCase() === this.filtro.toLowerCase());
+	var ret: Array<number> = new Array(0);
+	var id: number = 0;
+	for (let vehiculo of this.vehiculos)
+	{
+		if (vehiculo.marca.substring(0,this.filtro.length).toLowerCase() === this.filtro.toLowerCase())
+		{
+			ret.push(id);
+		}
+		id = id + 1;
+	}
+	return ret;
   }
 }
--- PRACTICA02/src/app/vehiculo.ts
+++ PRACTICA02/src/app/vehiculo.ts
@@ -1,3 +1,8 @@
+export enum EstadoCoche {
+	BUENO,
+	MALO
+}
+
 export class Vehiculo {
 	public foto: string;
 	public marca: string;
@@ -5,13 +10,15 @@ export class Vehiculo {
 	public anyo: Date;
 	public fechaVenta: Date;
 	public precio: number;
-	constructor(foto: string, marca: string, modelo: string, anyo: Date, fechaVenta: Date, precio: number){
+	public estado: EstadoCoche;
+	constructor(foto: string, marca: string, modelo: string, anyo: Date, fechaVenta: Date, precio: number, estado: EstadoCoche){
 		this.foto = foto;
 		this.marca = marca;
 		this.modelo = modelo;
 		this.anyo = anyo;
 		this.fechaVenta = fechaVenta;
 		this.precio = precio;
+		this.estado = estado;
 	}
 	getPVP(){
 		return this.precio * 1.21;
